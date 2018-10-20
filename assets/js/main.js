405eee000761defc09220e81c7fb99438397d6f5
Levi Villarreal <villarreallevi@gmail.com>
2018-10-20 18:09:28 -0500

    Add qr code generator to the app

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,22 @@ $(document).ready(function(){
         });
     });
 });
+
+$($('.button-submit')).click(function(){
+  var input = $('#email-form').val();
+
+  if (!validateEmail(input)) {
+    alert("Email is not valid");
+  } else {
+    var siteName = "file:///Users/levivillarreal/Desktop/HoldIt/post.html?email={";
+    $('#hide-on-submit').hide();
+    $('#qr-code').show();
+    var imageSrc = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + siteName + input + "}";
+    $('#qr-img').attr('src',imageSrc);
+  }
+});
+
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
